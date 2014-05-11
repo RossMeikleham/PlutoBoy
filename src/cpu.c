ab2383e083bee36563f1391d411ecf41e310a7ad
RossIain <rossmeikleham@hotmail.co.uk>
2014-05-11 16:39:22 +0100

    Added dissasembly information for dissasembling each opcode

--- src/cpu.c
+++ src/cpu.c
@@ -8,7 +8,6 @@
 #define IMMEDIATE_8_BIT get_mem(reg.PC+1)
 #define IMMEDIATE_16_BIT (get_mem((reg.PC)+2)<< 8) | get_mem((reg.PC+1))
 
-
 static uint8_t opcode;
 static int halt = 0;
 static int stop = 0;
@@ -49,18 +48,19 @@ typedef struct {
 typedef struct {
 
     Instruction const * const instruction_set; 
+    int const * words; /*  No of words per instruction */
     Instruction const * const ext_instruction_set; /* extended 0xCB instructions */
 
     /*  Separate the names from cycle/pointer information
      *  for closer cache locaility in performance. These are
      *  only used for dissasembly information. */
-    char const * const asm_name_set_1;
-    char const * const asm_name_set_2; 
+    char const * const *asm_name_set_1;
+    char const * const *asm_name_set_2; 
 
 } Instructions;
 
 
-static Instruction ins[256] = {
+static Instruction ins[UINT8_MAX+1] = {
     
     // 0x00 - 0x0F
     {4, NOP}, {12, LD_BC_IM}, {8, LD_memBC_A}, {8, INC_BC},     
@@ -159,7 +159,7 @@ static Instruction ins[256] = {
     {0 , NULL}, {0, NULL}, {8, CP_A_Im8}, {16, RST_38}
 };
 
-static Instruction ext_ins[256] = {
+static Instruction ext_ins[UINT8_MAX+1] = {
 
     {8, RLC_B}, {8, RLC_C}, {8, RLC_D},      {8, RLC_E}, 
     {8, RLC_H}, {8, RLC_L}, {16, RLC_memHL}, {8, RLC_A}, 
@@ -244,8 +244,197 @@ static Instruction ext_ins[256] = {
     {8, SET_H_7}, {8, SET_L_7}, {16, SET_memHL_7}, {8, SET_A_7}
 };
 
+/*  Number of words (bytes) per each instruction
+ *  All invallid instruction opcodes are given 1 word 
+ *  All extended instructions are 2 bytes, 1 for 0xCB opcode
+ *  and another for the specified extended opcode.*/
+static int ins_words[UINT8_MAX+1] = {
+
+    1,3,1,1,1,1,2,1,3,1,1,1,1,1,2,1,
+    2,3,1,1,1,1,2,1,2,1,1,1,1,1,2,1,
+    2,3,1,1,1,1,2,1,2,1,1,1,1,1,2,1,
+    2,3,1,1,1,1,2,1,2,1,1,1,1,1,2,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,3,3,3,1,2,1,1,1,3,2,3,3,2,1,
+    1,1,3,1,3,1,2,1,1,1,3,1,3,1,2,1,
+    2,1,2,1,1,1,2,1,2,1,3,1,1,1,2,1,
+    2,1,2,1,1,1,2,1,2,1,3,1,1,1,2,1
+};
+
+static char const * const asm_instruction_set[UINT8_MAX+1] = { 
+    "NOP", "LD BC,d16", "LD (BC),A", "INC BC", 
+    "INC B","DEC B", "LD B,d8", "RLCA", 
+    "LD (a16),SP", "ADD HL,BC", "LD A,(BC)", "DEC BC", 
+    "INC C","DEC C", "LD C,d8", "RRCA",
+    
+    "STOP 0", "LD DE,d16", "LD (DE),A", "INC DE",
+    "INC D", "DEC D", "LD D,d8", "RLA",
+    "JR r8", "ADD HL,DE", "LD A,(DE)", "DEC DE",
+    "INC E", "DEC E", "LD E,de", "RRA",
+     
+    "JR NZ,r8", "LD HL,d16", "LD (HL+),A", "INC HL",
+    "INC H", "DEC H", "LD H,d8", "DAA",
+    "JR Z,r8", "ADD HL,HL", "LD A,(HL+)", "DEC HL",
+    "INC L", "DEC L", "LD L,d8", "CPL",
+     
+    "JR NC,r8", "LD SP,d16", "LD (HL-),A", "INC SP",
+    "INC (HL)", "DEC (HL)", "LD (HL),d8", "SCF",
+    "JR C,r8", "ADD HL,SP","LD A,(HL-)", "DEC SP",
+    "INC A", "DEC A", "LD A,d8", "CCF",
+
+    "LD B,B", "LD B,C", "LD B,D", "LD B,E", 
+    "LD B,H","LD B,L", "LD B,(HL)", "LD B,A"
+    "LD C,B", "LD C,C", "LD C,D", "LD C,E",
+    "LD C,H", "LD C,L", "LD C,(HL)", "LD C,A", 
+
+    "LD D,B", "LD D,C", "LD D,D", "LD D,E", 
+    "LD D,H","LD D,L", "LD D,(HL)", "LD D,A"
+    "LD E,B", "LD E,C", "LD E,D", "LD E,E",
+    "LD E,H", "LD E,L", "LD E,(HL)", "LD E,A", 
+    
+    "LD H,B", "LD H,C", "LD H,D", "LD H,E", 
+    "LD H,H","LD H,L", "LD H,(HL)", "LD H,A"
+    "LD L,B", "LD L,C", "LD L,D", "LD L,E",
+    "LD L,H", "LD L,L", "LD L,(HL)", "LD L,A", 
+    
+    "LD (HL),B", "LD (HL),C", "LD (HL),D", "LD (HL),E", 
+    "LD (HL),H","LD (HL),L", "HALT", "LD (HL),A"
+    "LD A,B", "LD A,C", "LD A,D", "LD A,E",
+    "LD A,H", "LD A,L", "LD A,(HL)", "LD A,A", 
+
+    "ADD A,B", "ADD A,C", "ADD A,D", "ADD A,E",
+    "ADD A,H", "ADD A,L", "ADD A,(HL)", "ADD A,A",
+    "ADC A,B", "ADC A,C", "ADC A,D", "ADC A,E",
+    "ADC A,H", "ADC A,L", "ADC A,(HL)", "ADC A,A",
+
+    "SUB A,B", "SUB A,C", "SUB A,D", "SUB A,E",
+    "SUB A,H", "SUB A,L", "SUB A,(HL)", "SUB A,A",
+    "SBC A,B", "SBC A,C", "SBC A,D", "SBC A,E",
+    "SBC A,H", "SBC A,L", "SBC A,(HL)", "SBC A,A",
+ 
+    "AND A,B", "AND A,C", "AND A,D", "AND A,E",
+    "AND A,H", "AND A,L", "AND A,(HL)", "AND A,A",
+    "XOR A,B", "XOR A,C", "XOR A,D", "XOR A,E",
+    "XOR A,H", "XOR A,L", "XOR A,(HL)", "XOR A,A",
+
+    "OR A,B", "OR A,C", "OR A,D", "OR A,E",
+    "OR A,H", "OR A,L", "OR A,(HL)", "OR A,A",
+    "CP A,B", "CP A,C", "CP A,D", "CP A,E",
+    "CP A,H", "CP A,L", "CP A,(HL)", "CP A,A",
+
+    "RET NZ", "POP BC", "JP NZ,a16", "JP a16",
+    "CALL NZ,a16", "PUSH BC", "ADD A,d8", "RST 00H", 
+    "RET Z", "RET", "JP Z,a16", "EXT_INSTRUCTION", 
+    "CALL Z,a16", "CALL a16", "ADC A,d8", "RST 08H",
+
+    "RET NC", "POP DE", "JP NC,a16", "NONE", 
+    "CALL NC,a16", "PUSH DE", "SUB d8", "RST 10H", 
+    "RET C", "RETI", "JP C,a16", "NONE", 
+    "CALL C,a16", "NONE", "SBC A,d8", "RST 18H",
+
+    "LDH (a8), A", "POP HL", "LD (C),A", "NONE",
+    "NONE", "PUSH HL", "AND d8", "RST 20H",
+    "ADD SP,r8", "JP (HL)", "LD (a16),A", "NONE",
+    "NONE", "NONE", "XOR d8", "RST 28H",
+    
+    "LDH A,(a8)", "POP AF", "LD A,(C)", "DI",
+    "NONE", "PUSH AF", "OR d8", "RST 30H", 
+    "LD HL,SP+r8", "LD SP,HL", "LD A,(a16)", "EI",
+    "NONE", "NONE", "CP d8", "RST 38H"
+    
+};
+
+static char const * const asm_ext_instruction_set[UINT8_MAX+1] = {
+
+    "RLC B", "RLC C", "RLC D", "RLC E", 
+    "RLC H", "RLD L", "RLC (HL)", "RLC A", 
+
+    "RRC B", "RRC C", "RRC D", "RRC E", 
+    "RRC H", "RRC L", "RRC (HL)", "RRC A", 
+
+    "RL B", "RL C", "RL D", "RL E", 
+    "RL H", "RL L", "RL (HL)", "RL A", 
+
+    "RR B", "RR C", "RR D", "RR E", 
+    "RR H", "RR L", "RR (HL)", "RR A", 
+
+    "SLA B", "SLA C", "SLA D", "SLA E", 
+    "SLA H", "SLA L", "SLA (HL)", "SLA A", 
+
+    "SRA B", "SRA C", "SRA D", "SRA E", 
+    "SRA H", "SRA L", "SRA (HL)", "SRA A",
+     
+    "SWAP B", "SWAP C", "SWAP D", "SWAP E", 
+    "SWAP H", "SWAP L", "SWAP (HL)", "SWAP A",
+    
+    "SRL B", "SRL C", "SRL D", "SRL E", 
+    "SRL H", "SRL L", "SRL (HL)", "SRL A",
+    
+    "BIT 0,B", "BIT 0,C", "BIT 0,D", "BIT 0,E", 
+    "BIT 0,H", "BIT 0,L", "BIT 0,(HL)", "BIT 0,A"
+    "BIT 1,B", "BIT 1,C", "BIT 1,D", "BIT 1,E", 
+    "BIT 1,H", "BIT 1,L", "BIT 1,(HL)", "BIT 1,A"      
+    "BIT 2,B", "BIT 2,C", "BIT 2,D", "BIT 2,E", 
+    "BIT 2,H", "BIT 2,L", "BIT 2,(HL)", "BIT 2,A" 
+    "BIT 3,B", "BIT 3,C", "BIT 3,D", "BIT 3,E", 
+    "BIT 3,H", "BIT 3,L", "BIT 3,(HL)", "BIT 3,A" 
+    "BIT 4,B", "BIT 4,C", "BIT 4,D", "BIT 4,E", 
+    "BIT 4,H", "BIT 4,L", "BIT 4,(HL)", "BIT 4,A" 
+    "BIT 5,B", "BIT 5,C", "BIT 5,D", "BIT 5,E", 
+    "BIT 5,H", "BIT 5,L", "BIT 5,(HL)", "BIT 5,A" 
+    "BIT 6,B", "BIT 6,C", "BIT 6,D", "BIT 6,E", 
+    "BIT 6,H", "BIT 6,L", "BIT 6,(HL)", "BIT 6,A" 
+    "BIT 7,B", "BIT 7,C", "BIT 7,D", "BIT 7,E", 
+    "BIT 7,H", "BIT 7,L", "BIT 7,(HL)", "BIT 7,A",
+    
+    "RES 0,B", "RES 0,C", "RES 0,D", "RES 0,E",
+    "RES 0,H", "RES 0,L", "RES 0,(HL)", "RES 0,A",
+    "RES 1,B", "RES 1,C", "RES 1,D", "RES 1,E",
+    "RES 1,H", "RES 1,L", "RES 1,(HL)", "RES 1,A", 
+    "RES 2,B", "RES 2,C", "RES 2,D", "RES 2,E",
+    "RES 2,H", "RES 2,L", "RES 2,(HL)", "RES 2,A", 
+    "RES 3,B", "RES 3,C", "RES 3,D", "RES 3,E",
+    "RES 3,H", "RES 3,L", "RES 3,(HL)", "RES 3,A", 
+    "RES 4,B", "RES 4,C", "RES 4,D", "RES 4,E",
+    "RES 4,H", "RES 4,L", "RES 4,(HL)", "RES 4,A", 
+    "RES 5,B", "RES 5,C", "RES 5,D", "RES 5,E",
+    "RES 5,H", "RES 5,L", "RES 5,(HL)", "RES 5,A", 
+    "RES 6,B", "RES 6,C", "RES 6,D", "RES 6,E",
+    "RES 6,H", "RES 6,L", "RES 6,(HL)", "RES 6,A", 
+    "RES 7,B", "RES 7,C", "RES 7,D", "RES 7,E",
+    "RES 7,H", "RES 7,L", "RES 7,(HL)", "RES 7,A",
+
+    "SET 0,B", "SET 0,C", "SET 0,D", "SET 0,E",
+    "SET 0,H", "SET 0,L", "SET 0,(HL)", "SET 0,A",
+    "SET 1,B", "SET 1,C", "SET 1,D", "SET 1,E",
+    "SET 1,H", "SET 1,L", "SET 1,(HL)", "SET 1,A",
+    "SET 2,B", "SET 2,C", "SET 2,D", "SET 2,E",
+    "SET 2,H", "SET 2,L", "SET 2,(HL)", "SET 2,A",
+    "SET 3,B", "SET 3,C", "SET 3,D", "SET 3,E",
+    "SET 3,H", "SET 3,L", "SET 3,(HL)", "SET 3,A",
+    "SET 4,B", "SET 4,C", "SET 4,D", "SET 4,E",
+    "SET 4,H", "SET 4,L", "SET 4,(HL)", "SET 4,A",
+    "SET 5,B", "SET 5,C", "SET 5,D", "SET 5,E",
+    "SET 5,H", "SET 5,L", "SET 5,(HL)", "SET 5,A",
+    "SET 6,B", "SET 6,C", "SET 6,D", "SET 6,E",
+    "SET 6,H", "SET 6,L", "SET 6,(HL)", "SET 6,A",
+    "SET 7,B", "SET 7,C", "SET 7,D", "SET 7,E",
+    "SET 7,H", "SET 7,L", "SET 7,(HL)", "SET 7,A",
+
+};
+ 
 static Instructions instructions = {
-    .instruction_set = ins, .ext_instruction_set = ext_ins, NULL, NULL
+    .instruction_set = ins, .words = ins_words, 
+    .ext_instruction_set = ext_ins, 
+    .asm_name_set_1 = asm_instruction_set, 
+    .asm_name_set_2 = asm_ext_instruction_set
 };   
    
 
