2f127fb81dd6c8c9276db95efd1f9244b5b4381e
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-07-09 21:01:47 +0100

    Forgot to add timer module and header file int repository. They are now included.

---
+++ src/core/timers.c
@@ -0,0 +1,90 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  timers.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/07/14 20:10:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdint.h>
+#include "timers.h"
+#include "IO.h"
+
+#define DIV_TIMER_INC_FREQUENCY 16382
+
+//Possible timer increment timer_frequencies in hz
+static const long timer_frequencies[] = {4096, 262144, 65536, 16384}; 
+#define TIMER_FREQUENCIES_LEN sizeof (timer_frequencies) / sizeof (long)
+
+static long clock_speed = GB_CLOCK_SPEED_HZ;
+
+static long timer_frequency = 4096;
+static long timer_counter = 0;
+
+/*  Set and get the clockspeed in hz */
+void set_clock_speed(long hz) {
+    clock_speed = hz;
+}
+
+long get_clock_speed() {
+    return clock_speed;
+}
+
+/* Change the timer frequency to another of the possible
+ * frequencies, resets the timer_counter 
+ * If frequency number selected isn't valid then nothing happens*/
+void set_timer_frequency(unsigned int n) {
+    if (n < TIMER_FREQUENCIES_LEN) {
+        timer_frequency = timer_frequencies[n];
+        timer_counter = 0; // Need to reset timer counter if timer frequency changed
+    }
+}
+
+
+void update_divider_reg(long cycles) {
+    static long divider_counter = 0;
+
+    divider_counter += cycles;
+
+    // Increment div at a frequency of 16382hz
+    if (divider_counter >= clock_speed / DIV_TIMER_INC_FREQUENCY) {
+        increment_div();
+        divider_counter = 0;
+    }
+}
+
+
+void update_timers(long cycles) { 
+    uint8_t timer_control = io_get_mem(GLOBAL_TO_IO_ADDR(TAC_REG));
+    
+    update_divider_reg(cycles);
+    //Clock enabled
+    if ((timer_control & BIT_2) != 0) {
+        //If timer frequency has been changed need to reset
+        //the timer and update the current frequency
+        if (timer_frequencies[(timer_control & 3)] != timer_frequency) {
+            set_timer_frequency(timer_control & 3);
+        }
+        timer_counter += cycles;
+
+        if (timer_counter >= clock_speed / timer_frequency) {
+            increment_tima();
+            timer_counter = 0;
+        }
+    }
+}
+
+
+
+
+
---
+++ src/core/timers.h
@@ -0,0 +1,17 @@
+#ifndef TIMERS_H
+#define TIMERS_H
+
+#include <stdint.h>
+
+#define GB_CLOCK_SPEED_HZ  4194304 /* GameBoy Clock speed in HZ */
+#define SGB_CLOCK_SPEED_HZ 4295454 /* Super GameBoy Clock speed in HZ */
+#define CGB_CLOCK_SPEED_HZ 8388000 /* GameBoy Color Clock speed in HZ */
+
+
+/*  Set and get the clockspeed in hz */
+void set_clock_speed(long hz);
+long get_clock_speed();
+
+void update_timers(long cycles);
+
+#endif //TIMERS_H
