da78d57ca18341d6a31b9ff89c1176a9e093c159
RossMeikleham <RossMeikleham@hotmail.co.uk>
2015-09-10 16:22:50 +0100

    Fixed framerate issues on Windows version

--- src/shared_libs/SDL/framerate_SDL.c
+++ src/shared_libs/SDL/framerate_SDL.c
@@ -13,24 +13,56 @@ static uint64_t current_ticks = 0;
 
 #ifdef _WIN32
 #include <windows.h>
+#include <time.h>
 
-int gettimeofday( struct timeval *tv, struct timezone *tz ) {
-    time_t rawtime;
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+#else
+#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+#endif
 
-    time(&rawtime);
-    tv->tv_sec = (long)rawtime;
+struct timezone
+{
+	int  tz_minuteswest; /* minutes W of Greenwich */
+	int  tz_dsttime;     /* type of dst correction */
+};
 
-    LARGE_INTEGER tickPerSecond;
-    LARGE_INTEGER tick; // a point in time
+int gettimeofday(struct timeval *tv, struct timezone *tz)
+{
+	FILETIME ft;
+	unsigned __int64 tmpres = 0;
+	static int tzflag = 0;
 
-    QueryPerformanceFrequency(&tickPerSecond);
+	if (NULL != tv)
+	{
+		GetSystemTimeAsFileTime(&ft);
 
-    QueryPerformanceCounter(&tick);
+		tmpres |= ft.dwHighDateTime;
+		tmpres <<= 32;
+		tmpres |= ft.dwLowDateTime;
 
-    tv->tv_usec = (tick.QuadPart % tickPerSecond.QuadPart);
+		tmpres /= 10;  /*convert into microseconds*/
+		/*converting file time to unix epoch*/
+		tmpres -= DELTA_EPOCH_IN_MICROSECS;
+		tv->tv_sec = (long)(tmpres / 1000000UL);
+		tv->tv_usec = (long)(tmpres % 1000000UL);
+	}
 
-    return 0;
+	if (NULL != tz)
+	{
+		if (!tzflag)
+		{
+			_tzset();
+			tzflag++;
+		}
+		tz->tz_minuteswest = _timezone / 60;
+		tz->tz_dsttime = _daylight;
+	}
+
+	return 0;
 }
+
+
 
 #elif !defined(EMSCRIPTEN)
 #include <sys/time.h>
