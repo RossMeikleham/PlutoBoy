3c14971cd2f822679bd87274b094e08fa17ed046
RossMeikleham <rossmeikleham@hotmail.co.uk>
2015-12-17 01:15:22 +0000

    Update to fix name clash in windows build

--- src/shared_libs/get_time.c
+++ src/shared_libs/get_time.c
@@ -12,13 +12,13 @@
 #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
 #endif
 
-struct timezone
+static struct timezone
 {
 	int  tz_minuteswest; /* minutes W of Greenwich */
 	int  tz_dsttime;     /* type of dst correction */
 };
 
-int gettimeofday(struct timeval *tv, struct timezone *tz)
+static int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
 	FILETIME ft;
 	unsigned __int64 tmpres = 0;
