861b85e86e8b203a1a80928417338b7c134122dc
RossMeikleham <RossMeikleham@hotmail.co.uk>
2019-03-23 13:02:05 +0000

    Initial Switch port attempt, not tested on hardware yet

---
+++ src/platforms/Switch/main.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdio.h>
+#include <switch.h>
+#include "SDL/SDL.h"
+
+#include "../../core/emu.h"
+#include "../../core/serial_io.h"
+#include "../../core/mmu/mbc.h"
+#include "../../non_core/logger.h"
+
+void cleanup() {
+    
+    finalize_emu();
+    SDL_Quit();
+}
+
+int main(int argc, char **argv) {
+
+    int debug = 0;
+    int cgb_mode = 0;
+    char file_name[1024] = "rom.gb";
+
+    ClientOrServer cs = NO_CONNECT;
+    
+    set_log_level(LOG_INFO);
+
+    if (!init_emu(file_name, debug, !cgb_mode, cs)) {
+        log_message(LOG_ERROR, "Failed to initialize emulator\n");
+        cleanup();
+        return 1;
+    }
+    
+    log_message(LOG_INFO, "Running emu\n");
+    run();
+
+    write_SRAM();
+    cleanup();
+	return 0;
+}
