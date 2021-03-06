d68524e23b4e89bd962049ae89928697fdba1856
RossMeikleham <RossMeikleham@hotmail.co.uk>
2021-03-06 18:10:36 +0000

    Added missing bridging header

---
+++ build/WatchOS/Plutoboy/Plutoboy WatchKit Extension/c_src/Plutoboy WatchKit Extension-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#include "../../../../../src/core/emu.h"
+#include "../../../../../src/shared_libs/WatchOS/graphics_WatchOS_callbacks.h"
+#include "../../../../../src/shared_libs/WatchOS/joypad_set_WatchOS.h"
+#include "../../../../../src/shared_libs/WatchOS/sound_queue_WatchOS.h"
